ead25906701920f4ff06d4683356101ce01736bd
DylanBlanco <dylanblanco1198@gmail.com>
2024-10-21 04:34:44 +0200

    views.partials.footer.blade / inserito footer dinamicamente

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,69 @@
 <footer>
-    Footer
+    <div>
+        <div class="row">
+            {{-- DC Comics --}}
+            <div class="col">
+                <ul>
+                    <li>
+                        <h4>
+                            DC Comics
+                        </h4>
+                    </li>
+                    @foreach ($footerLinksDcComics as $element)
+                    <li>
+                        <a href="#"> {{ $element['name'] }} </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+            
+            {{-- DC --}}
+            <div class="col">
+                <ul>
+                    <li>
+                        <h4>
+                            DC
+                        </h4>
+                    </li>
+                    @foreach ($footerLinksDc as $element)
+                    <li>
+                        <a href="#"> {{ $element['name'] }} </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            {{-- Sites --}}
+            <div class="col">
+                <ul>
+                    <li>
+                        <h4>
+                            Sites
+                        </h4>
+                    </li>
+                    @foreach ($footerLinksSites as $element)
+                    <li>
+                        <a href="#"> {{ $element['name'] }} </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            {{-- Shop --}}
+            <div class="col">
+                <ul>
+                    <li>
+                        <h4>
+                            Shop
+                        </h4>
+                    </li>
+                    @foreach ($footerLinksShop as $element)
+                    <li>
+                        <a href="#"> {{ $element['name'] }} </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
